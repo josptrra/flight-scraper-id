aa1e6ced31e79d0c773f4a6e814c52312e1f6517
Julio Syah Putra <juliosp2107@gmail.com>
2025-06-08 22:02:16 +0700

    frontend canvas, react typescript tailwind

---
+++ frontend/flight-scraper/src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <p className="text-white text-2xl font-bold">doakan saya kuat.</p>
+    </div>
+  );
+}
+
+export default App;
